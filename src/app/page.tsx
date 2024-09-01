b64d1d109f624eb35d8ac89a3a18bdb569613bc7
Jay <original.jaysharma@gmail.com>
2024-09-01 21:31:06 +0530

    first project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+const Page = () => {
+    return (
+        <>
+            <main className="h-[100vh] w-full bg-black">
+              hello next!
+            </main>
+        </>
+    );
+};
+
+export default Page;
