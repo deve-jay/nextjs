a93e274a6dc4ae7be21233068d15aa1af73c88df
Jay <original.jaysharma@gmail.com>
2024-09-02 13:40:52 +0530

    new components added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Background from "@/components/Background";
+import VortexBg from "@/components/VortexBg";
 
 const Page = () => {
     return (
         <>
             <main className="h-[100vh] w-full bg-black">
               <Background />
+                <VortexBg />
             </main>
         </>
     );
